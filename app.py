002f127b3fc56cfb8378cecfe4c075edbb30eb5b
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-01-07 10:57:35 +0200

    pass the rate value to flask using url args

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -43,7 +43,8 @@ def book(book_isbn):
 
 @app.route("/rate/<int:book_id>")
 def rate(book_id):
-    return str(book_id)
+    value = request.args['value']
+    return str(book_id) + ' ' + str(value)
 
 
 
