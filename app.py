a00f8948e2b7b7912eac345ac413ad2f75f98932
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-06-08 00:14:07 +0200

    Check if the user has a rating

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def book(book_id):
         logged_user = login_session['user_id']
         user_rate = db.execute('''SELECT review_count FROM reviews WHERE book_id = :book_id and user_id = :user_id;''',
                               {"book_id": book_id, "user_id": login_session['user_id']}).fetchone()
-        if user_rate:
+        if user_rate.review_count:
             rate = user_rate.review_count
         else:
             rate = 0
